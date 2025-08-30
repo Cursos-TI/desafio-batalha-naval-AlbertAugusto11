0c6d1497a574547392e33129fdb6b95176dcc052
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2025-08-30 20:43:59 -0300

    NIVEL NOVATO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,109 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int esta_livre(int (*matriz)[10], int *posicoes, int length)
+{
+    if (length % 2 != 0)
+        return 0; // Garante que há pares completos
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    for (int i = 0; i < length; i += 2)
+    {
+        int linha = posicoes[i];
+        int coluna = posicoes[i + 1];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        if (matriz[linha][coluna] != 0)
+            return 0;
+    }
+    return 1;
+}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+int esta_dentro(int tamanhoLinha, int tamanhoColuna, int *posicoes, int length)
+{
+    if (length % 2 != 0)
+        return 0;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    for (int i = 0; i < length; i += 2)
+    {
+        int linha = posicoes[i];
+        int coluna = posicoes[i + 1];
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        if (linha < 0 || linha >= tamanhoLinha || coluna < 0 || coluna >= tamanhoColuna)
+            return 0;
+    }
+    return 1;
+}
+
+int posicionando(int (*matriz)[10], int *posicoes, int length, int esta_dentro, int esta_livre, int *barco)
+{
+    if (!esta_dentro)
+    {
+        printf("Erro: Coordenadas fora dos limites do tabuleiro.\n");
+        return 0;
+    }
+
+    if (!esta_livre)
+    {
+        printf("Erro: Coordenadas já estão ocupadas.\n");
+        return 0;
+    }
+
+    for (int i = 0, j = 0; i < length; i += 2, j++)
+    {
+        int linha = posicoes[i];
+        int coluna = posicoes[i + 1];
+        matriz[linha][coluna] = barco[j];
+    }
+
+    printf("Barco posicionado com sucesso!\n");
+    return 1;
+}
+
+void imprimirTabuleiro(int (*matriz)[10], int tamanhoLinha, int tamanhoColuna)
+{
+    for (int i = 0; i < tamanhoLinha; i++)
+    {
+        for (int j = 0; j < tamanhoColuna; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    // Tabuleiro 10x10
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    // Capturando o Tamanho das linhas e Colunas
+    int tamanho_linhas = sizeof(tabuleiro) / sizeof(tabuleiro[0]);
+    int tamanho_colunas = sizeof(tabuleiro[0]) / sizeof(tabuleiro[0][0]);
+
+    // Informações do 1º BARCO
+    int navio1[3] = {3, 3, 3};
+    int posicoes1[6] = {3, 3, 4, 3, 5, 3};
+    int x = esta_dentro(tamanho_linhas, tamanho_colunas, posicoes1, (sizeof(posicoes1) / sizeof(posicoes1[0])));
+    int y = esta_livre(tabuleiro, posicoes1, (sizeof(posicoes1) / sizeof(posicoes1[0])));
+    posicionando(tabuleiro, posicoes1, (sizeof(posicoes1) / sizeof(posicoes1[0])), x, y, navio1);
+
+    // Informações do 2º BARCO
+    int navio2[3] = {3, 3, 3};
+    int posicoes2[6] = {9, 2, 9, 3, 9, 4};
+    int a = esta_dentro(tamanho_linhas, tamanho_colunas, posicoes2, (sizeof(posicoes2) / sizeof(posicoes2[0])));
+    int b = esta_livre(tabuleiro, posicoes2, (sizeof(posicoes2) / sizeof(posicoes2[0])));
+    posicionando(tabuleiro, posicoes2, (sizeof(posicoes2) / sizeof(posicoes2[0])), a, b, navio2);
+
+    // Imprimindo o Tabuleiro
+    imprimirTabuleiro(tabuleiro, tamanho_linhas, tamanho_colunas);
 
     return 0;
 }
